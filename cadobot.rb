a2be2a49a59c9c33bb8f8a72b698dd409fcbcd7f
vsmart <fps2@st-andrews.ac.uk>
2014-06-17 21:00:50 +0100

    fixed indentation

--- cadobot.rb
+++ cadobot.rb
@@ -1,32 +1,32 @@
 require "cinch" 
 
 cadobot = Cinch::Bot.new do
-	configure do |c|
-		c.server = "irc.freenode.net"
-		c.channels = ["#avocadosandwich"]
-	end
+  configure do |c|
+    c.server = "irc.freenode.net"
+    c.channels = ["#avocadosandwich"]
+  end
 
-	on :message, "hello" do |m|
-		m.reply "hello to you too" 
-	end
+  on :message, "hello" do |m|
+    m.reply "hello to you too" 
+  end
 
-	# On join event 
+  # On join event 
 
-	on :join do |m|
-		m.reply "Someone joined" 
-		if (m.user.nick != @bot.nick)
-			if (m.user.nick =~ /^gr[e3]g.*$/)
-				m.reply "Hi Greg"
-		  	 else 
-				m.reply "Hi " + m.user.nick
-			end 
-		end
-	end
+  on :join do |m|
+    m.reply "Someone joined" 
+    if (m.user.nick != @bot.nick)
+      if (m.user.nick =~ /^gr[e3]g.*$/)
+        m.reply "Hi Greg"
+        else 
+        m.reply "Hi " + m.user.nick
+      end      
+    end
+  end
 
-	# On asking if franziska is there	
-	on :message, /^[Ff]ranziska[?]?$/ do |m|
-		m.reply "She is not here."
-	end
+  # On asking if franziska is there 
+  on :message, /^[Ff]ranziska[?]?$/ do |m|
+    m.reply "She is not here."
+  end
 end
 
 cadobot.start
