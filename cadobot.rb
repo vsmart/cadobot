1e3b95656d84aeb946d739931efe6b17aaff6806
vsmart <fps2@st-andrews.ac.uk>
2014-06-17 20:40:36 +0100

    improved join messages

--- cadobot.rb
+++ cadobot.rb
@@ -9,6 +9,23 @@ cadobot = Cinch::Bot.new do
 	on :message, "hello" do |m|
 		m.reply "hello to you too" 
 	end
+
+	# On join event 
+	on :join do |m|
+		m.reply "Someone joined" 
+		if (m.user.nick != @bot.nick)
+			if (m.user.nick =~ /^gr[e3]g.*$/)
+				m.reply "Hi Greg"
+		  	 else 
+				m.reply "Hi " + m.user.nick
+			end 
+		end
+	end
+
+	# On asking if franziska is there	
+	on :message, /^[Ff]ranziska[?]?$/ do |m|
+		m.reply "She is not here."
+	end
 end
 
 cadobot.start
